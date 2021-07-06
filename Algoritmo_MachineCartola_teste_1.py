0448ce2d23fbfe121a51633fdce8c8f5b849f203
junior-2020 <64149300+junior-2020@users.noreply.github.com>
2021-07-06 20:15:10 -0300

    Dados tratados e plots feitos, partindo para clustering

--- Algoritmo_MachineCartola_teste_1.py
+++ Algoritmo_MachineCartola_teste_1.py
@@ -3,30 +3,77 @@ import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
 
+from locale import atof, setlocale, LC_NUMERIC
+
+setlocale(LC_NUMERIC, '')
+
 ########################################################################################################################################################################
 '''
 Aquisição dos dados e visualização dos gráficos da base de dados que será utilizada
 '''
 ########################################################################################################################################################################
 
+f = lambda x: atof(x)
 
-dados = pd.read_csv('clustering.csv')
 
-print(dados)
+dados = pd.read_csv('Dados_xls_CartolaFC_teste_1.csv', sep=';', converters={'pontos_num':f, 'preco_num':f, 'variacao_num': f, 'media_num':f })
 
-rendaAnual = 'ApplicantIncome'
-valorEmprestimo = 'LoanAmount'
+print(dados.head())
 
-dados_eixo_x = dados[[valorEmprestimo, rendaAnual]]
+print(dados.describe(include='float64'))
+
+
+pontuacaoAtleta = 'pontos_num'
+precoAtleta = 'preco_num'
+mediaAtleta = 'media_num'
+partidasJogadas = 'jogos_num'
+scoutId = 'scout'
+qtdeScout = 'qtde_scout'
+clubeId = 'clube_id'
+posicaoId = 'posicao_id'
+
+dados_eixo_x_scout = dados[[scoutId, qtdeScout]]
 
 # Fazendo os gráficos para visualização
 
+print(dados_eixo_x_scout)
 
-
-plt.scatter(dados_eixo_x[rendaAnual], dados_eixo_x[valorEmprestimo], c='black')
-plt.xlabel('Renda Anual')
-plt.ylabel('Valor do empréstimo (em milhares)')
+plt.scatter(dados_eixo_x_scout[scoutId], dados_eixo_x_scout[qtdeScout], c='black')
+plt.xlabel('Tipo de Scout')
+plt.ylabel('Quantidade do scout')
 plt.show()
+
+dados_eixo_x_atleta = dados[[pontuacaoAtleta, precoAtleta, partidasJogadas]]
+print(dados_eixo_x_atleta)
+
+plt.scatter(dados_eixo_x_atleta[partidasJogadas], dados_eixo_x_atleta[pontuacaoAtleta], c='black')
+plt.xlabel('Partidas jogadas')
+plt.ylabel('Pontuação do atleta')
+plt.show()
+
+
+dados_eixo_x_atleta = dados[[mediaAtleta, posicaoId]]
+print(dados_eixo_x_atleta)
+
+plt.scatter(dados_eixo_x_atleta[posicaoId], dados_eixo_x_atleta[mediaAtleta], c='black')
+plt.xlabel('Posição em campo')
+plt.ylabel('Média do atleta')
+plt.show()
+
+'''
+Este gráfico merece estudo, parece promissor
+'''
+
+plt.scatter(dados_eixo_x_atleta[mediaAtleta], dados_eixo_x_scout[qtdeScout], c='black')
+plt.xlabel('Média do atleta')
+plt.ylabel('Quantidade do scout')
+plt.show()
+
+# for index, linha in dados_eixo_x_scout.iterrows():
+#     if linha[scoutId] == 'PI':
+#         print(dados_eixo_x_atleta.at[index, mediaAtleta])
+        
+
 
 ########################################################################################################################################################################
 '''
@@ -34,21 +81,24 @@ Seleção do numéro de aglomerados (clusters) e centróides de cada aglomerado
 '''
 ########################################################################################################################################################################
 
-K = 3
+# K = 3
 
-centroides = (dados_eixo_x.sample(K))
-plt.scatter(dados_eixo_x[rendaAnual], dados_eixo_x[valorEmprestimo], c='black')
-plt.scatter(centroides[rendaAnual], centroides[valorEmprestimo], c='red')
-plt.xlabel('Renda Anual')
-plt.ylabel('Valor Empréstimo (em milhares)')
-plt.show()
+# centroides = (dados_eixo_x.sample(K))
+# plt.scatter(dados_eixo_x[rendaAnual], dados_eixo_x[valorEmprestimo], c='black')
+# plt.scatter(centroides[rendaAnual], centroides[valorEmprestimo], c='red')
+# plt.xlabel('Renda Anual')
+# plt.ylabel('Valor Empréstimo (em milhares)')
+# plt.show()
 
-print(dados_eixo_x)
+# print(dados_eixo_x)
 
 ########################################################################################################################################################################
 '''
 Associação dos pontos ao centroide mais próximo e recalculando os centroides
 '''
+########################################################################################################################################################################
+
+# Não funciona
 ########################################################################################################################################################################
 
 # diferenca = 1
@@ -88,45 +138,53 @@ Associação dos pontos ao centroide mais próximo e recalculando os centroides
 #                 posicao = i+1
 #         dados_c.append(posicao)
 
-diff = 1
-j=0
+########################################################################################################################################################################
+########################################################################################################################################################################
 
-while(diff!=0):
-    XD=dados_eixo_x
-    i=1
-    for index1,row_c in centroides.iterrows():
-        ED=[]
-        for index2,row_d in XD.iterrows():
-            d1=(row_c["ApplicantIncome"]-row_d["ApplicantIncome"])**2
-            d2=(row_c["LoanAmount"]-row_d["LoanAmount"])**2
-            d=np.sqrt(d1+d2)
-            ED.append(d)
-        dados_eixo_x[i]=ED
-        i=i+1
+#Aqui funciona
+########################################################################################################################################################################
 
-    C=[]
-    for index,row in dados_eixo_x.iterrows():
-        min_dist=row[1]
-        pos=1
-        for i in range(K):
-            if row[i+1] < min_dist:
-                min_dist = row[i+1]
-                pos=i+1
-        C.append(pos)
-    dados_eixo_x["Cluster"]=C
-    Centroids_new = dados_eixo_x.groupby(["Cluster"]).mean()[["LoanAmount","ApplicantIncome"]]
-    if j == 0:
-        diff=1
-        j=j+1
-    else:
-        diff = (Centroids_new['LoanAmount'] - centroides['LoanAmount']).sum() + (Centroids_new['ApplicantIncome'] - centroides['ApplicantIncome']).sum()
-        print(diff.sum())
-    centroides = dados_eixo_x.groupby(["Cluster"]).mean()[["LoanAmount","ApplicantIncome"]]
 
-print(dados_eixo_x)
+
+# diff = 1
+# j=0
+
+# while(diff!=0):
+#     XD=dados_eixo_x
+#     i=1
+#     for index1,row_c in centroides.iterrows():
+#         ED=[]
+#         for index2,row_d in XD.iterrows():
+#             d1=(row_c["ApplicantIncome"]-row_d["ApplicantIncome"])**2
+#             d2=(row_c["LoanAmount"]-row_d["LoanAmount"])**2
+#             d=np.sqrt(d1+d2)
+#             ED.append(d)
+#         dados_eixo_x[i]=ED
+#         i=i+1
+
+#     C=[]
+#     for index,row in dados_eixo_x.iterrows():
+#         min_dist=row[1]
+#         pos=1
+#         for i in range(K):
+#             if row[i+1] < min_dist:
+#                 min_dist = row[i+1]
+#                 pos=i+1
+#         C.append(pos)
+#     dados_eixo_x["Cluster"]=C
+#     Centroids_new = dados_eixo_x.groupby(["Cluster"]).mean()[["LoanAmount","ApplicantIncome"]]
+#     if j == 0:
+#         diff=1
+#         j=j+1
+#     else:
+#         diff = (Centroids_new['LoanAmount'] - centroides['LoanAmount']).sum() + (Centroids_new['ApplicantIncome'] - centroides['ApplicantIncome']).sum()
+#         print(diff.sum())
+#     centroides = dados_eixo_x.groupby(["Cluster"]).mean()[["LoanAmount","ApplicantIncome"]]
+
+# print(dados_eixo_x)
     
-plt.scatter(dados_eixo_x[rendaAnual], dados_eixo_x[valorEmprestimo], c='black')
-plt.scatter(centroides[rendaAnual], centroides[valorEmprestimo], c='red')
-plt.xlabel('Renda Anual')
-plt.ylabel('Valor Empréstimo (em milhares)')
-plt.show()
+# plt.scatter(dados_eixo_x[rendaAnual], dados_eixo_x[valorEmprestimo], c='black')
+# plt.scatter(centroides[rendaAnual], centroides[valorEmprestimo], c='red')
+# plt.xlabel('Renda Anual')
+# plt.ylabel('Valor Empréstimo (em milhares)')
+# plt.show()
