e4e7bcdb3f04908b88627c0fdeff9f03138efc13
junior-2020 <64149300+junior-2020@users.noreply.github.com>
2021-08-01 17:19:46 -0300

    Divisão dos algoritmos e ajustes by gus

--- Algoritmo_MachineCartola_teste_1.py
+++ Algoritmo_MachineCartola_teste_1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
-
+import clustering_script
 
 
 ########################################################################################################################################################################
@@ -10,187 +10,90 @@ import matplotlib.pyplot as plt
 Aquisição dos dados e visualização dos gráficos da base de dados que será utilizada
 '''
 ########################################################################################################################################################################
-from locale import atof, setlocale, LC_NUMERIC
+# from locale import atof, setlocale, LC_NUMERIC
 
-setlocale(LC_NUMERIC, '')
-f = lambda x: atof(x)
-
-
-dados = pd.read_csv('Dados_xls_CartolaFC_teste_1.csv', sep=';', converters={'pontos_num':f, 'preco_num':f, 'variacao_num': f, 'media_num':f })
-
-print(dados.head())
-
-print(dados.describe(include='float64'))
+# setlocale(LC_NUMERIC, '')
+# f = lambda x: atof(x)
 
 
-pontuacaoAtleta = 'pontos_num'
-precoAtleta = 'preco_num'
-mediaAtleta = 'media_num'
-partidasJogadas = 'jogos_num'
-scoutId = 'scout'
-qtdeScout = 'qtde_scout'
-clubeId = 'clube_id'
-posicaoId = 'posicao_id'
+dados = pd.read_csv('atletaspontuacao.csv', sep=',', converters={'Id':int, 'Atleta_id':int, 'Rodada_id': int})
+#, 'Pontos':f, 'Preço':f, 'Variação':f, 'Média':f })
 
-dados_eixo_x_scout = dados[[scoutId, qtdeScout]]
+print("Imprimindo as primeiras entradas dos dados: \n", dados.head())
+
+print("Fazendo a descrição estatística dos dados: \n", dados.describe(include='float64'))
+
+
+pontuacaoAtleta = 'Pontos'
+precoAtleta = 'Preço'
+mediaAtleta = 'Média'
+variacaoAtleta = 'Variação'
+# partidasJogadas = 'jogos_num'
+# scoutId = 'scout'
+# qtdeScout = 'qtde_scout'
+# clubeId = 'clube_id'
+# posicaoId = 'posicao_id'
+atletaId = 'Atleta_id'
+rodadaId = 'Rodada_id'
+
 
 # Fazendo os gráficos para visualização
 
-print(dados_eixo_x_scout)
+dados_eixo_x_atleta = dados[[atletaId, pontuacaoAtleta, precoAtleta, mediaAtleta, rodadaId]]
+print("Imprimindo os dados selecionados: \n", dados_eixo_x_atleta)
 
-plt.scatter(dados_eixo_x_scout[scoutId], dados_eixo_x_scout[qtdeScout], c='black')
-plt.xlabel('Tipo de Scout')
-plt.ylabel('Quantidade do scout')
-plt.show()
-
-dados_eixo_x_atleta = dados[[pontuacaoAtleta, precoAtleta, partidasJogadas, mediaAtleta, posicaoId]]
-print(dados_eixo_x_atleta)
-
-plt.scatter(dados_eixo_x_atleta[partidasJogadas], dados_eixo_x_atleta[pontuacaoAtleta], c='black')
-plt.scatter(dados_eixo_x_atleta[partidasJogadas], dados_eixo_x_atleta[mediaAtleta], c='red')
-plt.xlabel('Partidas jogadas')
-plt.ylabel('Pontuação do atleta(b) / Media(r)')
-plt.show()
+# plt.scatter(dados_eixo_x_atleta[rodadaId], dados_eixo_x_atleta[pontuacaoAtleta], c='black')
+# plt.scatter(dados_eixo_x_atleta[rodadaId], dados_eixo_x_atleta[mediaAtleta], c='red')
+# plt.xlabel('Número da rodada')
+# plt.ylabel('Pontuação do atleta(b) / Media(r)')
+# plt.show()
 
 
-# dados_eixo_x_atleta = dados[[mediaAtleta, posicaoId]]
-print(dados_eixo_x_atleta)
-
-plt.scatter(dados_eixo_x_atleta[posicaoId], dados_eixo_x_atleta[mediaAtleta], c='black')
-plt.xlabel('Posição em campo')
+plt.scatter(dados_eixo_x_atleta[precoAtleta], dados_eixo_x_atleta[mediaAtleta], c='black')
+plt.xlabel('Preço do atleta')
 plt.ylabel('Média do atleta')
-plt.show()
+plt.show() # Bom gráfico!
+
 
 plt.scatter(dados_eixo_x_atleta[mediaAtleta], dados_eixo_x_atleta[pontuacaoAtleta], c='black')
 plt.xlabel('Média do atleta')
 plt.ylabel('Pontuação do atleta')
+plt.show() # Bom gráfico!
+
+# atletas_unicos = dados_eixo_x_atleta.groupby(atletaId)
+# cor = (rd.random(), rd.random(), rd.random())
+# print(atletas_unicos.shape)
+ids_passadas = []
+
+
+from cycler import cycler
+
+plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y', 'c', 'k']) *
+                           cycler('linestyle', ['-', '--', ':', '-.'])))
+plt.figure()
+for id in dados_eixo_x_atleta[atletaId]:
+    if id not in ids_passadas:
+        atleta_atual = dados_eixo_x_atleta[dados_eixo_x_atleta[atletaId] == id]
+        plt.plot(atleta_atual[rodadaId], atleta_atual[pontuacaoAtleta])
+        ids_passadas.append(id)
+        if len(ids_passadas) > 50:
+            break
+plt.xlabel('Número da rodada')
+plt.ylabel('Pontuação de cada atleta')
 plt.show()
 
-'''
-Este gráfico merece estudo, parece promissor
-'''
-
-plt.scatter(dados_eixo_x_atleta[mediaAtleta], dados_eixo_x_scout[qtdeScout], c='black')
-plt.xlabel('Média do atleta')
-plt.ylabel('Quantidade do scout')
-plt.show()
 
 # for index, linha in dados_eixo_x_scout.iterrows():
 #     if linha[scoutId] == 'PI':
 #         print(dados_eixo_x_atleta.at[index, mediaAtleta])
         
+clusters_atletas = clustering_script.Clusters(dados)
+# print(clusters_atletas.dados)
+clusters_atletas.formar_clusters()
+# clusters_atletas.plotar_clusters()
+contador = 0
 
-
-########################################################################################################################################################################
-'''
-Seleção do numéro de aglomerados (clusters) e centróides de cada aglomerado
-'''
-########################################################################################################################################################################
-
-# K = 3
-
-# centroides = (dados_eixo_x.sample(K))
-# plt.scatter(dados_eixo_x[rendaAnual], dados_eixo_x[valorEmprestimo], c='black')
-# plt.scatter(centroides[rendaAnual], centroides[valorEmprestimo], c='red')
-# plt.xlabel('Renda Anual')
-# plt.ylabel('Valor Empréstimo (em milhares)')
-# plt.show()
-
-# print(dados_eixo_x)
-
-########################################################################################################################################################################
-'''
-Associação dos pontos ao centroide mais próximo e recalculando os centroides
-'''
-########################################################################################################################################################################
-
-# Não funciona
-########################################################################################################################################################################
-
-# diferenca = 1
-# j = 0
-
-# while diferenca != 0:
-#     dados_xd = dados_eixo_x
-#     i = 1
-#     for index1, linha_c in centroides.iterrows():
-#         dados_ed = []
-#         for index2, linha_d in dados_xd.iterrows():
-#             distancia1 = (linha_c[rendaAnual] - linha_d[rendaAnual]) ** 2
-#             distancia2 = (linha_c[valorEmprestimo] - linha_d[valorEmprestimo]) ** 2
-#             distanciaHipotenusa = np.sqrt(distancia1 + distancia2)
-
-#             dados_ed.append(distanciaHipotenusa)
-
-#         print(f' A variável i é {i}')
-#         print(dados_eixo_x[1])
-#         dados_eixo_x[i] = dados_ed
-#         i += 1
-
-#     dados_c = []
-
-#     diferenca = 0
-
-# print(type(dados_eixo_x))
-# print()
-
-
-#     for index, linha in dados_eixo_x.iterrows():
-#         distancia_minima = linha[1]
-#         posicao = 1
-#         for i in range(K):
-#             if linha[i+1] < distancia_minima:
-#                 distancia_minima = linha[i+1]
-#                 posicao = i+1
-#         dados_c.append(posicao)
-
-########################################################################################################################################################################
-########################################################################################################################################################################
-
-#Aqui funciona
-########################################################################################################################################################################
-
-
-
-# diff = 1
-# j=0
-
-# while(diff!=0):
-#     XD=dados_eixo_x
-#     i=1
-#     for index1,row_c in centroides.iterrows():
-#         ED=[]
-#         for index2,row_d in XD.iterrows():
-#             d1=(row_c["ApplicantIncome"]-row_d["ApplicantIncome"])**2
-#             d2=(row_c["LoanAmount"]-row_d["LoanAmount"])**2
-#             d=np.sqrt(d1+d2)
-#             ED.append(d)
-#         dados_eixo_x[i]=ED
-#         i=i+1
-
-#     C=[]
-#     for index,row in dados_eixo_x.iterrows():
-#         min_dist=row[1]
-#         pos=1
-#         for i in range(K):
-#             if row[i+1] < min_dist:
-#                 min_dist = row[i+1]
-#                 pos=i+1
-#         C.append(pos)
-#     dados_eixo_x["Cluster"]=C
-#     Centroids_new = dados_eixo_x.groupby(["Cluster"]).mean()[["LoanAmount","ApplicantIncome"]]
-#     if j == 0:
-#         diff=1
-#         j=j+1
-#     else:
-#         diff = (Centroids_new['LoanAmount'] - centroides['LoanAmount']).sum() + (Centroids_new['ApplicantIncome'] - centroides['ApplicantIncome']).sum()
-#         print(diff.sum())
-#     centroides = dados_eixo_x.groupby(["Cluster"]).mean()[["LoanAmount","ApplicantIncome"]]
-
-# print(dados_eixo_x)
-    
-# plt.scatter(dados_eixo_x[rendaAnual], dados_eixo_x[valorEmprestimo], c='black')
-# plt.scatter(centroides[rendaAnual], centroides[valorEmprestimo], c='red')
-# plt.xlabel('Renda Anual')
-# plt.ylabel('Valor Empréstimo (em milhares)')
-# plt.show()
+for i in dados_eixo_x_atleta:
+    if dados_eixo_x_atleta[dados_eixo_x_atleta[rodadaId] == 13]:
+        contador += 1
+        print(contador)
---
+++ clustering_script.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+import random as rd
+import matplotlib.pyplot as plt
+
+class Clusters():
+    def __init__(self, dados, K=15):
+        self.K = K
+        self.dados = dados
+
+        self.pontuacaoAtleta = 'Pontos'
+        self.precoAtleta = 'Preço'
+        self.mediaAtleta = 'Média'
+        self.variacaoAtleta = 'Variação'
+        self.atletaId = 'Atleta_id'
+        self.rodadaId = 'Rodada_id'
+
+    def formar_clusters(self):
+        ########################################################################################################################################################################
+        '''
+        Seleção do numéro de aglomerados (clusters) e centróides de cada aglomerado
+        '''
+        ########################################################################################################################################################################
+
+        # K = 15
+
+        centroides = (self.dados.sample(self.K))
+        # plt.scatter(dados_eixo_x[rendaAnual], dados_eixo_x[valorEmprestimo], c='black')
+        # plt.scatter(centroides[rendaAnual], centroides[valorEmprestimo], c='red')
+        # plt.xlabel('Renda Anual')
+        # plt.ylabel('Valor Empréstimo (em milhares)')
+        # plt.show()
+
+        # print(dados_eixo_x)
+
+        ########################################################################################################################################################################
+        '''
+        Associação dos pontos ao centroide mais próximo e recalculando os centroides
+        '''
+        ########################################################################################################################################################################
+
+        ########################################################################################################################################################################
+
+        #Aqui funciona
+        ########################################################################################################################################################################
+
+        diff = 1
+        j=0
+
+        while(diff>1):
+            XD=self.dados
+            i=1
+            for index1,row_c in centroides.iterrows():
+                ED=[]
+                for index2,row_d in XD.iterrows():
+                    d1=(row_c[self.mediaAtleta]-row_d[self.mediaAtleta])**2
+                    d2=(row_c[self.precoAtleta]-row_d[self.precoAtleta])**2
+                    d=np.sqrt(d1+d2)
+                    ED.append(d)
+                self.dados[i]=ED
+                i=i+1
+
+            C=[]
+            for index,row in self.dados.iterrows():
+                min_dist=row[1]
+                pos=1
+                for i in range(self.K):
+                    if row[i+1] < min_dist:
+                        min_dist = row[i+1]
+                        pos=i+1
+                C.append(pos)
+            self.dados["Cluster"]=C
+            Centroids_new = self.dados.groupby(["Cluster"]).mean()[[self.precoAtleta,self.mediaAtleta]]
+            if j == 0:
+                diff=1
+                j=j+1
+            else:
+                diff = (Centroids_new[self.precoAtleta] - centroides[self.precoAtleta]).sum() + (Centroids_new[self.mediaAtleta] - centroides[self.mediaAtleta]).sum()
+                print(diff.sum())
+            centroides = self.dados.groupby(["Cluster"]).mean()[[self.precoAtleta,self.mediaAtleta]]
+
+        self.centroides = centroides
+        # print(dados_eixo_x)
+    def plotar_clusters(self):
+        plt.scatter(self.dados[self.mediaAtleta], self.dados[self.precoAtleta], c='black')
+        plt.scatter(self.centroides[self.mediaAtleta], self.centroides[self.precoAtleta], c='red')
+        plt.xlabel('Média do atleta')
+        plt.ylabel('Preço do atleta')
+        plt.show()
